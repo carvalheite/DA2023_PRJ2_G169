b370a84f80b6b7d75a6788426392e49c60433b1f
Gonçalo Carvalho <goncalo27carvalho@gmail.com>
2024-05-15 17:36:24 +0100

    data parsing and graphs are fully implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "Graph.h"
+#include "FileReader.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Graph<int> g;
+    FileReader fr(&g);
+    fr.loadRealGraph(3);
+
+    for (auto vertex : g.getVertexSet()){
+        for (auto edge : vertex->getAdj()) {
+            cout << vertex->getInfo() << " -> " << edge->getDest()->getInfo() << " : " << edge->getWeight() << endl;
+        }
+    }
+
     return 0;
 }
